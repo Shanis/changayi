2d6cd169c900f6aa88dc9dfcb761e475c7e4c873
Shanis <shanis.km@gmail.com>
2011-09-26 14:37:42 -0700

    Initial - 09/26/2011

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Changayi::Application.config.secret_token = 'a060fcadb2f546a1e730bfeab506f6f7b8d901f19ea47927882d7a2dc298f144e5cd687686439e9d9aef9b223138bd8a5a82df61b04f00d5861dedf2a1950205'
